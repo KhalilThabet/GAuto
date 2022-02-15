6cc8dbc660a729c4ae4ae48db99c135999920768
khalilthabet <khalilthabet@outlook.com>
2022-02-15 23:17:29 +0100

    Code Refactoring

--- frontend/src/AdminSection/navbar/navbaremp.js
+++ frontend/src/AdminSection/navbar/navbaremp.js
@@ -1,28 +1,25 @@
-import {React,  Component } from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.css';
-import logo from './icon/logo.png'
+import { React, Component } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import logo from "./icon/logo.png";
 
-class NavBar extends Component
-{
-    render(){
-        return(
-        
-            <div className='navbar'>
-               <nav className='container'>
-                   <img src={logo} id='logo' alt='logo' ></img>
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <nav className="container">
+          <img src={logo} id="logo" alt="logo"></img>
 
-                    <ul>
-                        <br></br>
-                       <li>
-                         <Link to="/gcondidat" >Candidate Management</Link> 
-                         <Link to="/vehicule" >Vehicules Management</Link> 
-                      </li>
-                 </ul>
-              </nav>
-            </div>
-          
-        )
-    }
+          <ul>
+            <br></br>
+            <li>
+              <Link to="/gcondidat">Candidate Management</Link>
+              <Link to="/vehicule">Vehicules Management</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
-export default NavBar
+export default NavBar;
