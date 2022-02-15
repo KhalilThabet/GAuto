4348684b596afc24d891698486250044085b2120
khalilthabet <khalilthabet@outlook.com>
2022-02-15 23:18:27 +0100

    Directory Reordering and code refactoring

---
+++ frontend/src/Routing/AnimationLoading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AnimationLoading.css";
+import lod from "./Assets/loader.gif";
+export default function Charge() {
+  //REFRESH PAGE
+  return (
+    <div className={"Loader"}>
+      <h1 id="loadtext">Please Wait ...</h1>
+      <img src={lod} alt="chargement" id="loadgif"></img>
+    </div>
+  );
+}
---
+++ frontend/src/Routing/Reload.js
@@ -0,0 +1,17 @@
+import { React } from "react";
+import { useHistory } from "react-router";
+import Charge from "./AnimationLoading";
+
+//This function makes sure the databases are refreshed
+export default function Reload({Path}) {
+  const history = useHistory();
+  setTimeout(() => {
+    history.push(Path);
+  }, 2000);
+
+  return (
+    <div>
+      <Charge />
+    </div>
+  );
+}
